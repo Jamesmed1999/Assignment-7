c110ef9dcca64d65159669e9c776e39188c15fde
Jamesmed1999 <Jamesmed1999@gmail.com>
2021-01-14 13:55:30 -0500

    More bug fixes

--- src/components/searchField.js
+++ src/components/searchField.js
@@ -1,47 +1,53 @@
-import React from 'react'
+import React from "react";
 
+function SearchField(props) {
+  function onSubmit(event,value) {
+    event.preventDefault()
+    console.log(value);
 
-
-
-
- function SearchField(props) {
-    function onSubmit(value)
-    {
+    if (value === 1) {
+      fetch(
+        "https://api.giphy.com/v1/gifs/trending?api_key=cD5OcHcYruMyAgmdd08Pem21ciNrHk8A"
+      )
+        .then((response) => response.json())
+        .then((res) => props.setGifs(res.data));
+    } else if (value === 2) {
+      fetch(
+        "https://api.giphy.com/v1/gifs/random?api_key=cD5OcHcYruMyAgmdd08Pem21ciNrHk8A"
+      )
+        .then((response) => response.json())
+        .then((res) => props.setGifs(res.data));
         
-        console.log(value)
-        
-        if(value === 1)
-        {
-            props.setGiflink(
-                'http://api.giphy.com/v1/gifs/trending?api_key=cD5OcHcYruMyAgmdd08Pem21ciNrHk8A'
-            )
-        }
-        else if(value === 2)
-        {
-            props.setGiflink(
-                'http://api.giphy.com/v1/gifs/random?api_key=cD5OcHcYruMyAgmdd08Pem21ciNrHk8A'
-            )
-        }
-        else{
-            let value = document.forms["form1"]["forChar"].value
-            let search = 'http://api.giphy.com/v1/gifs/search?q=' +value + '&api_key=cD5OcHcYruMyAgmdd08Pem21ciNrHk8A' 
-           search = search.replace(/\s+/g, '-')
-            console.log("the search link: ", search)
-            props.setGiflink(search)
-        }
+    } else {
+    
+      let val= document.forms["form1"]["forChar"].value;
+      let search =
+        "https://api.giphy.com/v1/gifs/search?q=" +
+        val +
+        "&api_key=cD5OcHcYruMyAgmdd08Pem21ciNrHk8A";
+      search = search.replace(/\s+/g, "-");
+
+      fetch(search)
+        .then((response) => response.json())
+        .then((res) => props.setGifs(res.data));
     }
+  }
 
-    return (
-        <div className ='searchDiv'>
-            <img src="search.jpg" alt="Looking for gifs" class="resize"/>
-            <h1 className ='gif-search' >Search gifs </h1>
-        <form id = "form1" name = "form">
-        <input type = "text" id = "forChar"  ></input>
-        </form>
-        <button onClick = {() => onSubmit(0)}>Search</button>
-        <button onClick = {() => onSubmit(1)} value = '1'>trending Search</button>
-        <button onClick = {() => onSubmit(2)} value = "2">Random Search</button>
-        </div>
-    )
+  return (
+    <div className="searchDiv">
+      <img src="search.jpg" alt="Looking for gifs" class="resize" />
+      <h1 className="gif-search">Search gifs </h1>
+      <form id="form1" name="form">
+        <input type="text" id="forChar"></input>
+      </form>
+      <button onClick={(e) => onSubmit(e,0)}>Search</button>
+      <button onClick={(e) => onSubmit(e,1)} value="1">
+        trending Search
+      </button>
+      <button onClick={(e) => onSubmit(e,2)} value="2">
+        Random Search
+      </button>
+    </div>
+  );
 }
-export default SearchField
+export default SearchField;
